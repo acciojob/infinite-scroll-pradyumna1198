b075172895795b911bc2f29710a426403a8280eb
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-08-23 14:07:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 //your code here!
 
+document.addEventListener(function() {
+	const infi = document.getElementById("infi-list");
+
+	for(let i=1;i<=10;i++){
+		const listItem =   document.createElement("li");
+		listItem.textContent = `Item ${i}`;
+		infi.appendChild(listItem);
+	}
+
+	function addItem(){
+		for(let i=1;i<=2;i++){
+			const listItem = document.createElement("li");
+			listItem.textContent = `Item ${i}`;
+			infi.appendChild(listItem);
+		}
+	}
+
+	infi.addEventListener(function(){
+		const scrolltop = infi.scrollTop;
+		const scrollHeight= infi.scrollHeight;
+		const clientHeight = infi.clientHeight;
+
+
+		if(scrolltop + clientHeight >= scrollHeight){
+			addMoreItems();
+		}
+	});
+});
+
