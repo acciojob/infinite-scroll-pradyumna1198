e5114016b55b58eb5f6611e5493f4f9868554987
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-08-23 19:22:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 //your code here!
-let list = document.getElementById("infi-list");
+let list = document.querySelector("#infi-list");
 console.log(list);
 
-for(let i=1;i<=10;i++){
-	list.innerHTML += `<li>Item ${i}</li>`
+for(let i=1; i<=10; i++){
+  list.innerHTML += `<li>Item ${i}</li>`
 }
-let n=10;
 
-list.addEventListener("scroll" ()=>{
+let n = 10;
 
-	if(list.scrollheight-list.scrollTop-list.clientHeight <1){
-		list.innerHTML += `
-	<li>Item ${n+1}</li>
-	 <li>Item ${n+2}</li>
-	`
-		n+=2;
-	}
+list.addEventListener("scroll", ()=>{
+  // console.log("used scrolled the list");
+  if(list.scrollHeight - list.scrollTop - list.clientHeight < 1){
+    // console.log("you reached the end");
+    list.innerHTML += `
+    <li>Item ${n+1}</li>
+    <li>Item ${n+2}</li>
+    `
+    n+=2;
+  }
 })
-
 
 
